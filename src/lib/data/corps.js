a846cbd9ae2f6bf638e22d14eaf9c4485d9a52a0
Anais <anais.duval@etu.iut-tlse3.fr>
2024-12-06 00:51:26 +0100

    Ajout de texte pour la page corps

--- src/lib/data/corps.js
+++ src/lib/data/corps.js
@@ -3,7 +3,10 @@ export const organes = [
 		id: 'coeur',
 		title: 'Le Cœur',
 		content:
-			'Lorem impsum dolor'
+			`<p>La base de l'humanité, le symbole de la passion. Le cœur humain pompe avec ardeur, nourrit le corps tout entier, et on retrouve la même ardeur, bien que moins visible. L’océan, aussi vaste et abyssal soit-il, tourne et se renouvelle à la manière d’un sang azur grâce à la circulation thermohaline, ou on peut également parler de circulation méridienne de retournement, et ce système si technique façonné par Mère Nature ne doit être détraqué.</p>
+			<p>Malheureusement, cette sagesse n’a pas forcément été instruite, ou peut-être oubliée, et son bon fonctionnement en a fini déséquilibré. Le ralentissement de la circulation méridienne de retournement, selon de nombreux experts, s’accompagne de conséquences désastreuses comme des saisons plus marquées, des tempêtes plus fréquentes et une augmentation du niveau des mers. Ces symptômes sont ceux d’un cœur de la mer malade, assurément, tout comme un cœur humain malade se ferait sentir par des affaiblissements, des malaises, et d'autres symptômes.</p>
+			<p>Tout comme l’homme doit prendre soin de son organe le plus important, il faut faire attention à ne pas perturber le flux de la circulation thermohaline en mer, et ça passe par de la prévention (comme on le fait maintenant) et de l’action (de vous principalement !)</p>
+			`
 	},
 
 	{
@@ -23,12 +26,17 @@ export const organes = [
 		id: 'poumons',
 		title: 'Les Poumons',
 		content:
-			'Lorem impsum dolor'
+			`<p>Le besoin le plus vital du corps humain est l’oxygène. En tant que tel, notre corps nécessite plus que tout d’être oxygéné au travers du sang, tout comme la circulation thermohaline fait traverser les eaux, et un fléau qui frappe tout autant les océans que nos corps peut être la désoxygénation. Et si votre cerveau ne recevait plus d’oxygène hein ? Vous le savez clairement, ça entraînerait de graves conséquences, court ou long terme.</p>
+			<p>Et il faut bien s’imaginer que c’est quelque chose qui arrive très fréquemment pour notre océan, l’accumulation de dioxyde de carbone crée des zones de désoxygénation graves, qui entraînent le réchauffement climatique (c’en est une cause reconnue) et aussi l’eutrophisation. L’eutrophisation, il s’agit de zones ultra concentrées en nutriments, qui sont dangereuses car ça sature et empêche la propagation de ces nutriments. Imaginez que votre corps soit saturé de nutriments en un seul endroit, laissant le reste affamé, c’est ce qui se passe.</p>`
 	},
     {
 		id: 'foie',
 		title: 'Le Foie',
 		content:
-			'Lorem impsum dolor'
+			`<p>Notre corps est bien fait : il est équipé de ses propres usines de purification : les reins et le foie.</p>
+			<p>D’abord, le foie transforme les substances toxiques (alcool, médicaments, produits chimiques) en composés moins nocifs pour être éliminés via les reins ou les intestins. Le foie produit aussi de la bile, un liquide qui aide à éliminer les déchets liposolubles (cholestérol, toxines). De leur côté, les reins filtrent le sang pour en éliminer les déchets. Le sang arrive dans les reins et passe à travers les glomérules, où l'eau, les sels, les déchets (comme l'urée), et d'autres petites molécules sont filtrés. Les déchets et substances inutiles sont ensuite acheminés vers la vessie sous forme d’urine. Les bonnes substances (glucose, acides aminés…) sont ensuite réabsorbées par le sang.</p>
+			<p>En cas de dysfonctionnement de ce système, c’est la catastrophe : les toxines s’accumulent dans le corps, entraînant un jaunissement de la peau et une diminution de la capacité immunitaire.</p>
+			<p>De la même façon, l’océan a ses mécanismes de nettoyage intégrés. Les océans sont vastes et en constante agitation grâce aux courants marins, aux vagues et aux marées. Ces mouvements permettent de diluer les polluants, réduisant leur concentration locale. Ensuite, des micro-organismes marins jouent un rôle clé en dégradant certains polluants organiques, tels que les hydrocarbures. Ces organismes transforment les substances toxiques en composés moins nocifs ou inoffensifs.</p>
+			<p>Malheureusement, cette mécanique bien réglée est mise à mal par le changement climatique qui, en augmentant la température de l’eau, augmente son absorption de CO2. Cela cause une acidification des océans qui est nocive pour les organismes marins, dont ceux qui sont essentiels à la régulation des polluants ! Et là, l’océan tombe malade…</p>`
 	},
 ];
