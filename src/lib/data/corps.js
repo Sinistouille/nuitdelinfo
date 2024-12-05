53bcf561c24e2e9020c6c3ce26a4de7c3c1882c8
alex <lescot.alexg@gmail.com>
2024-12-06 00:27:08 +0100

    data fetch

---
+++ src/lib/data/corps.js
@@ -0,0 +1,34 @@
+export const organes = [
+	{
+		id: 'coeur',
+		title: 'Le Cœur',
+		content:
+			'Lorem impsum dolor'
+	},
+
+	{
+		id: 'rate',
+		title: 'La Rate',
+		content:
+			'Lorem impsum dolor'
+	},
+
+	{
+		id: 'cerveau',
+		title: 'Le Cerveau',
+		content:
+			'Lorem impsum dolor'
+	},
+    {
+		id: 'poumons',
+		title: 'Les Poumons',
+		content:
+			'Lorem impsum dolor'
+	},
+    {
+		id: 'foie',
+		title: 'Le Foie',
+		content:
+			'Lorem impsum dolor'
+	},
+];
