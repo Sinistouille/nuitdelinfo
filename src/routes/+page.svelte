a207a6f64467e66ca86d05671b4e78c5f19b558d
alex <lescot.alexg@gmail.com>
2024-12-05 22:04:28 +0100

    toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,72 @@
 <script>
-    import { onMount } from 'svelte';
-    import { gsap } from 'gsap';
-    import logo from '$lib/assets/Lyreco_Logo.png'
+  import { onMount } from "svelte";
+  import gsap from "gsap";
+  import logo from '$lib/assets/Lyreco_Logo.png'
 
-    onMount(() => {
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-        
-        var tl = gsap.timeline();
+  let tl;
+  let isPaused = false;
 
-        tl.from(".circle-image", {
-            opacity: 0, 
-            y: height, 
-            duration: 1 })
-        .to(".circle-image", {
-            duration: 1,  
-            x: width/2,
-            y: 0
-        })
-        .to(".circle-image", {
-            duration : 1,
-            rotation : 180,
-            x: width/2, 
-            y: height/1.1,
-            rotate: 27
-        })
-        .to(".circle-image", {
-            duration : 1,
-            rotation : 180,
-            x: width/1.1, 
-            y: height/1.1
-        })
-        .to(".circle-image", {
-            duration : 1,
-            rotation : 180,
-            x: 0, 
-            y: height/1.1
-        })
-        .to(".circle-image", {
-            duration : 2,
-            rotation : 180,
-            x: width/1.1, 
-            y: 0
-        });
-        tl.yoyo(true).repeat(-1);
-        
+  // Initialisation de l'animation
+  onMount(() => {
+    const height = window.innerHeight;
+    const width = window.innerWidth;
+
+    tl = gsap.timeline({
+      repeat: -1,
+      yoyo: true,
     });
+
+    tl.to(".circle-image", {
+      opacity: 1,
+      y: height,
+      duration: 1,
+    })
+      .to(".circle-image", {
+        duration: 2,
+        rotation: 27,
+        x: 0,
+        y: 0,
+      }, 1.5)
+      .to(".circle-image", {
+        duration: 2,
+        x: width / 2,
+        y: 0,
+      }, 2)
+      .to(".circle-image", {
+        duration: 2,
+        rotation: 180,
+        x: width / 2,
+        y: height / 1.1,
+      }, 3)
+      .to(".circle-image", {
+        duration: 2,
+        rotation: 180,
+        x: width / 1.1,
+        y: height / 1.1,
+      }, 4.5)
+      .to(".circle-image", {
+        duration: 2,
+        rotation: 180,
+        x: 0,
+        y: height / 1.1,
+      }, 6)
+      .to(".circle-image", {
+        duration: 2,
+        rotation: 180,
+        x: width / 1.1,
+        y: 0,
+      }, 6.4);
+  });
+
+  // Fonction pour gérer le clic
+  function toggleAnimation() {
+    if (isPaused) {
+      tl.play(); 
+    } else {
+      tl.pause(); 
+    }
+    isPaused = !isPaused;
+  }
 </script>
 
 <div class="circle-image-container">
@@ -53,7 +74,7 @@
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-    <img src={logo} class="circle-image" alt="Circling Image" />
+    <img src={logo} class="circle-image" alt="Circling Image" onclick={toggleAnimation}/>
     
     
     <div class="box gradient-green green"></div>
