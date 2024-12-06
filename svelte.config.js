20e48c6704d26e3a56947ade34e17904b6f8f4fa
alex <lescot.alexg@gmail.com>
2024-12-06 06:56:45 +0100

    deployeuh

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
-
-    kit: {
-        adapter: adapter(),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
-        }
-    }
+	kit: {
+		// Adapter pour le rendu statique
+		adapter: adapter(),
+		// Configuration des chemins
+		paths: {
+			base: '/sveltekit-github-pages'
+		}
+	}
 };
 
 export default config;
